bee8afc2ea1f4bcbe8001bb8ef95bdf270e46ddb
keevanese <kqtang@cpp.edu>
2025-12-10 00:57:05 -0800

    Delete demo 2 directory

--- demo 2/signExt.v
+++
@@ -1,9 +0,0 @@
-`default_nettype none
-module signExt(
-    input [15:0] immediate, //16-bit in
-    output [31:0] extended  //extended 32-bit out
-    );
-
-    assign extended = {{16{immediate[15]}}, immediate};
-
-endmodule
